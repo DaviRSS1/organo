0626f4113f1e2fc6cfb30711775df702ef1ec963
Davi <131279133+DaviRSS1@users.noreply.github.com>
2023-06-14 20:55:21 -0300

    Upando arquivos da versao 1v1

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,38 @@
 import './Colaborador.css'
+import { GrClose } from 'react-icons/gr'
+import { AiOutlineHeart, AiFillHeart} from 'react-icons/ai'
 
 const Colaborador = (props) =>{
+
+    function favoritar() {
+        props.aoFavoritar(props.id)
+    }   
+
+    const propsFavorito ={
+        size: 25,
+        onClick: favoritar
+    }
+
+    console.log(props)
+
     return(
         <div className='colaborador'>
-            <div className='cabecalho' style={{backgroundColor: props.corDeFundo}}>
+            <GrClose 
+                size={25} 
+                className='deletar' 
+                onClick={() => props.aoDeletar(props.id)}
+            />
+            <div className='cabecalho' style={{backgroundColor: props.cor}}>
                 <img src={props.imagem} alt={props.nome}></img>
             </div>
             <div className='rodape'>
                 <h4>{props.nome}</h4>
                 <h5>{props.cargo}</h5>
+                <div className='favoritar'>
+                    {props.favorito
+                        ? <AiFillHeart {...propsFavorito} color='red'/> 
+                        : <AiOutlineHeart {...propsFavorito}/>}
+                </div>
             </div>
         </div>
     )
